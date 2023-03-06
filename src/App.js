53c7773f65b8616e188cb9e3715700e0138d47a5
josecho97 <201611779@est.umss.edu>
2023-03-06 14:18:06 -0400

    The module basics of react, was finished

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useState } from "react";
+import { Footer } from "./components/Footer";
+import Header from './components/Header';
+import { Producto } from "./components/Producto";
+import { Carrito } from './components/Carrito';
 function App() {
+
+
+  //crear listado de productos
+  const [productos, setProductos] = useState([
+    { id: 1, nombre: 'Camisa', precio: 50 },
+    { id: 2, nombre: 'Polera', precio: 30 },
+    { id: 3, nombre: 'Short', precio: 60 },
+    { id: 4, nombre: 'Pantalon', precio: 10 },
+  ]);
+
+  //Obtener la fecha 
+  const fecha = new Date().getFullYear();
+
+
+  //State para un carrito de compras
+  const [carrito, agregarProducto] = useState([]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header
+        titulo='Tienda Virtual'
+      />
+
+      <h1>Lista de Productos</h1>
+      {productos.map(producto => (
+        <Producto
+          key={producto.id}
+          producto={producto}
+          carrito={carrito}
+          productos={productos}
+          agregarProducto={agregarProducto}
+        />
+      ))}
+      <Carrito
+        carrito={carrito}
+        agregarProducto={agregarProducto}
+      />
+      <Footer
+        fecha={fecha}
+      />
+    </>
   );
 }
 
---
+++ src/components/Footer.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const Footer = ({fecha})  => {
+  return (
+    <footer>Todos los derechos reservados &copy; {fecha}</footer> 
+  )
+}
